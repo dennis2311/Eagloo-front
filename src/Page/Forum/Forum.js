434a491d98a46bf49a5ffbcbc417a3774c7de609
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2021-01-29 15:00:06 +0900

    포럼 헤드 실험

--- src/Page/Forum/Forum.js
+++ src/Page/Forum/Forum.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import styled from "styled-components";
 import ForumHead from "./ForumHead";
 import ForumBody from "./ForumBody";
 import ForumFoot from "./ForumFoot";
 
+const server = "https://eaglooserver.herokuapp.com";
+
 const ForumContainer = styled.div`
-    width: 100%;
     display: flex;
+    min-width: 960px;
+    width: 90%;
     flex-direction: column;
     align-items: center;
+    border: 2px solid blueviolet;
 `;
 
 function Forum() {
+    const category = useRef("");
+    const [totalThreads, setTotalThreads] = useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [currentThreads, setCurrentThreads] = useState([]);
+
+    useEffect(() => {
+        async function getThreads() {
+            try {
+                // const { data } = await axios.get(`${server}/threads`);
+                // setTotalThreads(data.length);
+            } catch (error) {}
+        }
+        getThreads();
+    }, [category]);
+
     return (
         <ForumContainer>
-            <ForumHead />
-            <ForumBody />
-            <ForumFoot />
+            <ForumHead category={category} />
+            <ForumBody currentThreads={currentThreads} />
+            <ForumFoot
+                currentPage={currentPage}
+                setCurrentPage={setCurrentPage}
+                setCurrentThreads={setCurrentThreads}
+            />
         </ForumContainer>
     );
 }
