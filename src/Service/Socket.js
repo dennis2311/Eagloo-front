c7bf3745031c9bafb01d03165b3f933ec654d432
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2021-02-18 19:39:09 +0900

    소켓 연결 URL 변경

--- src/Service/Socket.js
+++ src/Service/Socket.js
@@ -1,6 +1,6 @@
 import io from "socket.io-client";
 import { createContext } from "react";
 
-export const socket = io.connect(`http://localhost:8000`);
-// export const socket = io.connect(`https://eaglooserver.herokuapp.com`);
+// export const socket = io.connect(`http://localhost:8000`);
+export const socket = io.connect(`https://eaglooserver.herokuapp.com`);
 export const SocketContext = createContext();
