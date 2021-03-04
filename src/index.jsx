9649f98baed34270c53eed4a9bedc41c9574fd5f
KimKwangHoon <rhkdgns9489@gmail.com>
2021-03-04 14:12:17 +0900

    CRA → parcel 번들러 교체 (#1)

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import App from "./Router/App";
+import App from "./Router/App";
 
-ReactDOM.render(<>hello world</>, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
